9601f638dfc6e44da48a38a5651f22a0e164b18e
dgoodare <55392744+dgoodare@users.noreply.github.com>
2021-03-24 20:10:55 +0000

    defined some actions
    need to implement some decision making algorithm that will choose which actions are possible for a given patient

--- patient.py
+++ patient.py
@@ -7,27 +7,29 @@
 class Patient:
     #constructor
     def __init__(self, age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, target):
-        self.age = int(age)#age of the patient
-        self.sex = int(sex)#sex of the patient (1 = male; 0 = female)
-        self.cp = int(cp)#chest pain type (0,1,2,3)
+        #self.age = int(age)#age of the patient
+        #self.sex = int(sex)#sex of the patient (1 = male; 0 = female)
+        #self.cp = int(cp)#chest pain type (0,1,2,3)
         self.trestbps = int(trestbps)#resting blood pressure
         self.chol = int(chol)#serum cholesterol
         self.fbs = int(fbs)#fasting blood sugar (is it greater than 120mg/dl? 1 = true; 0 = false)
-        self.restecg = int(restecg)#resting ECG (0,1,2)
-        self.thalach = int(thalach)#maximum heart rate achieved
-        self.exang = int(exang)#exercise enduce angina (1 = yes; 0 = no)
-        self.oldpeak = float(oldpeak)#ST depression induced by exercise relative to rest
-        self.slope = int(slope)#slope of the peak exercise ST segment (1 = upsloping; 2 = flat; 3 = downsloping)
-        self.ca = int(ca)#the number of major vessels(0,1,2,3)
+        #self.restecg = int(restecg)#resting ECG (0,1,2)
+        #self.thalach = int(thalach)#maximum heart rate achieved
+        #self.exang = int(exang)#exercise enduce angina (1 = yes; 0 = no)
+        #self.oldpeak = float(oldpeak)#ST depression induced by exercise relative to rest
+        #self.slope = int(slope)#slope of the peak exercise ST segment (1 = upsloping; 2 = flat; 3 = downsloping)
+        #self.ca = int(ca)#the number of major vessels(0,1,2,3)
         self.thal = int(thal)#a blood disorder known as 'thalassemia' (3 = normal; 6 = fixed defect; 7 = reversable defect)
-        self.target = int(target)#pressence of heart disease (0 = no; 1 = yes)
+        #self.target = int(target)#pressence of heart disease (0 = no; 1 = yes)
 
     #prints the information
     def printPatient(self):
-        print("Age: ", self.age, "Sex: ", self.sex, "Chest Pain: ", self.cp, "Blood Pressure: ", self.trestbps, "Cholestorol: ", self.chol, "FBS: ", self.fbs, "Resting ECG: ", self.restecg, "Max heart rate: ", self.thalach, "Exercise enduced angina: ", self.exang, "Oldpeak: ", self.oldpeak, "Slope: ", self.slope, "Major blood vessels: ", self.ca, "Thalassemia: ", self.thal, "Target: ", self.target)
+        #print("Age: ", self.age, "Sex: ", self.sex, "Chest Pain: ", self.cp, "Blood Pressure: ", self.trestbps, "Cholestorol: ", self.chol, "FBS: ", self.fbs, "Resting ECG: ", self.restecg, "Max heart rate: ", self.thalach, "Exercise enduced angina: ", self.exang, "Oldpeak: ", self.oldpeak, "Slope: ", self.slope, "Major blood vessels: ", self.ca, "Thalassemia: ", self.thal, "Target: ", self.target)
+        print("[Blood Pressure (mm/Hg):", self.trestbps, "]    [Serum Cholesterol (mg/dl):", self.chol, "]    [Is Fasting Blood Sugar above 120mg/dl? (1=true, 0=false):", self.fbs, "]    [Thalassemia (1=Normal, 2=Fixed Defect, 3=Reversable Defect)", self.thal , ']')
         print('\n')
 
     ## -------- Actions -------- ##
+    """
     #at the moment, these are still 'placeholder actions' that don't adhere to any real science
 
     #each will lower patient's corresponding attribute by an arbitrary amount
@@ -52,3 +54,30 @@ class Patient:
         #only lower cp if it is > 0
         if(self.cp > 0):
             self.cp -= 1
+    """
+## ---- Exercises ---- ##
+    def swimming(self):#action 1
+        self.trestbps -= 4
+        self.chol *= 0.9
+
+    def jogging(self):#action 2
+        self.trestbps -= 3
+        self.chol *= 0.8
+
+    def briskWalking(self):#action 3
+        self.trestbps -= 1
+        self.chol *= 0.95
+
+## ---- Diets ---- ##
+# Ideally, these would directly affect blood sugar levels. 
+# But since fbs is represented as a boolean value that 
+# dictates if it is greater than 120mg/dl (1 = true), 
+# the change from true to false will be based on how deep 
+# in the search true we are (how long the patient has been on the diet).
+    def DASHdiet(self):#action 4
+        self.trestbps -= 6
+        self.chol *= 0.85
+
+    def Meditarranean(self):#action 5
+        self.trestbps -= 5
+        self.chol *= 0.8
